f08622e5914746367a9d5110e376d5cd338ead3e
valientjuno <jessedoake91@gmail.com>
2025-09-19 09:40:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# routes_main-v2
+
+This project is a reboil, sites could be reached at
+
+https://github.com/valientjuno/routes_main-v2
+
+or
+
+https://routes-main-v2.onrender.com
+
+<!-- routes follow as is -->
+
+/awesomeFunction
+/tooeleTech
+/getAllStudents
+/getSingleStudent
+/createStudent
+/deleteStudent
+/gives
+
+Enjoy!
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const routes = require("express").Router();
+const { required } = require("nodemon/lib/config");
+const myController = require("../controllers/index");
+
+routes.get("/", myController.awesomeFunction);
+routes.get("/ttech", myController.tooeleTech);
+
+routes.use("/students", require("./students"));
+
+routes.use("/auth", require("./auth"));
+
+routes.use("/myRoute", myController.myRoute);
+module.exports = routes;
